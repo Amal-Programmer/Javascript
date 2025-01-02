4b26daa360c2e586dde1af6bd65ca20842bb8afb
Amal <amal.otmani@usmba.ac.ma>
2025-01-02 15:41:11 +0100

    Lab: writing your first javascript function

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,12 @@
+let length;
+let width;
+
+//parseFloat built-in JavaScript function that takes a string and tries to convert it into a floating-point number
+//.value retrieves the string entered into the input field associated with that element
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    
+    let area = length*width;
+    document.getElementById('result').innerText = 'The area of the rectangle is: ${area}';
+}
